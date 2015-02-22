8faf9314d8f47924821ba43fe1c77152eb3f51e9
Mpiern01 <Marcpierne@gmail.com>
2015-02-22 09:39:39 -0800

    Edits to Assignment 2 Peer Eval

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a cacheble matrix type. Has the ability to cache the
+## result and look it up in the future to improve performance.
 
-## Write a short comment describing this function
+## Create matrix type.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setCache <- function(matinv) inv <<- matinv
+  
+  getCache <- function() inv
+  
+  list(set = set, get = get, setCache = setCache, getCache = getCache)
 }
 
 
-## Write a short comment describing this function
+## Return inverse of cachedmatrix x. This looks up the cache if it is available. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getCache()
+  
+  
+  if(!is.null(inv)) {
+    message("Getting cached matrix.")
+    return(inv)
+  }
+  
+  data <- x$get()
+  
+  inv <- solve(data, ...)
+  
+  x$setCache(inv)
+  
+  inv
 }
