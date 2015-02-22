261c69dc3af1f4fda398a26f5cfa54ec25334bc2
Mpiern01 <Marcpierne@gmail.com>
2015-02-22 09:53:30 -0800

    edited comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Create a cacheble matrix type. Has the ability to cache the
 ## result and look it up in the future to improve performance.
 
-## Create matrix type.
+## Create the first matrix type.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
