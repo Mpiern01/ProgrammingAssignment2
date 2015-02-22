70a6eefd5e4e6138162e683ec3074aae59a7b028
Mpiern01 <Marcpierne@gmail.com>
2015-02-22 09:49:18 -0800

    First Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -11,32 +11,5 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   
-  get <- function() x
   
-  setCache <- function(matinv) inv <<- matinv
-  
-  getCache <- function() inv
-  
-  list(set = set, get = get, setCache = setCache, getCache = getCache)
-}
 
-
-## Return inverse of cachedmatrix x. This looks up the cache if it is available. 
-
-cacheSolve <- function(x, ...) {
-  inv <- x$getCache()
-  
-  
-  if(!is.null(inv)) {
-    message("Getting cached matrix.")
-    return(inv)
-  }
-  
-  data <- x$get()
-  
-  inv <- solve(data, ...)
-  
-  x$setCache(inv)
-  
-  inv
-}
